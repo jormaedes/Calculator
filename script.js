64180e5d7472f1d9d410ea07d092f5f9caac692c
Jormaedes Luís <jormahed@gmail.com>
2025-11-18 16:48:55 +0100

    terminando a logica do projeto

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const btnDelete = document.querySelector("#delete");
 const point = document.querySelector("#point");
 const operators = [...document.querySelectorAll('.operator')];
 const percent = document.querySelector("#percentage");
+const equal = document.querySelector("#equal");
+const expression = document.querySelector("#expression");
 
 let signal = "";
 let stash = 0;
-
+let control = 0;
 
 percent.addEventListener('click', ()=>{
 	let value;
@@ -26,9 +28,19 @@ point.addEventListener('click', ()=>{
 	if (display.textContent.includes('.'))
 		return ;
 	display.textContent += '.';
+	control = 1;
 });
 
 btnDelete.addEventListener('click', ()=>{
+	if ((display.textContent == '' && expression.textContent != '')
+		|| ( expression.textContent != '' && display.textContent == '0'))
+	{
+		console.log("cheguei")
+		expression.textContent = '';
+		display.textContent = stash;
+		signal = '';
+		return ;
+	}
 	if (display.textContent == '0')
 		return ;
 	if (display.textContent.length === 1)
@@ -42,21 +54,31 @@ btnDelete.addEventListener('click', ()=>{
 
 clean.addEventListener('click', ()=>{
 	display.textContent = '0';
+	expression.textContent = "";
+	stash = 0;
 });
 
 numbers.map(el=>{
 	el.addEventListener('click', ()=>{
-		if(display.textContent == '0')
+		if(display.textContent == '0' || control == 0)
 			display.textContent = el.textContent;
 		else
 			display.textContent += el.textContent;
+		control = 1;
 	});
 });
 
 operators.map(el =>{
 	el.addEventListener("click", ()=>{
-		stash = display.textContent;
+		if (display.textContent[display.textContent.length - 1] == '.')
+			return ;
+		if (expression.textContent != '' && display.textContent != '')
+			execute();
 		signal = el.textContent;
+		if (display.textContent != '')
+			stash = display.textContent;
+		expression.textContent = stash + " " + signal;
+		display.textContent = '';
 	});
 });
 
@@ -82,4 +104,32 @@ function percentage(a) {
 	return (divide(a, 100));
 }
 
+function stringToNumber(n){
+	if (n.includes('.'))
+		return (parseFloat(n));
+	return (parseInt(n));
+}
 
+function execute(){
+	if (display.textContent[display.textContent.length - 1] == '.' || display.textContent == '')
+			return ;
+	const a = stringToNumber(stash);
+	const b = stringToNumber(display.textContent);
+	expression.textContent = `${a} ${signal} ${b}`;
+	if (signal == '+')
+		display.textContent = add(a, b).toString();
+	else if (signal == '-')
+		display.textContent = subtract(a, b).toString();
+	else if (signal == 'x')
+		display.textContent = multiply(a, b).toString();
+	else if (signal == '/')
+		display.textContent = divide(a, b).toString();
+}
+
+equal.addEventListener('click', ()=>{
+	if (display.textContent == '')
+		return ;
+	execute();
+	expression.textContent = '';
+	control = 0;
+});
